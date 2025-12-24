f62989dddf1f258eb08cf7c6935bc4434ce6c5ac
millereric819-oss <millereric819@gmail.com>
2025-12-24 15:48:17 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Red River HVAC Services</h1>
+      <p>Site is deploying correctly.</p>
+    </main>
+  );
+}
 
